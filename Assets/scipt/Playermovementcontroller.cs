7883ec2bb61cffb9b4f5b2c52879c21b6d493e07
kartik <kartikawaji@gmail.com>
2026-07-26 22:07:29 +0530

    added player controller script and edited some script

---
+++ Assets/scipt/Playermovementcontroller.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using GridPuzzle.Gameplay;
+
+namespace GridPuzzle.Gameplay
+{
+    public class PlayerMovementController : MonoBehaviour
+    {
+        [SerializeField] private float cellSize = 1f;
+        [SerializeField] private AnimationCurve easeCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+        public bool IsMoving { get; private set; }
+        private Coroutine _activeMove;
+
+        public void SnapTo(Vector2Int gridPos)
+        {
+            if (_activeMove != null) { StopCoroutine(_activeMove); _activeMove = null; }
+            IsMoving = false;
+            transform.position = GridCoordinateUtil.GridToWorld(gridPos, cellSize);
+        }
+
+        public void MoveTo(Vector2Int targetGridPos, float duration, Action onComplete = null)
+        {
+            if (_activeMove != null) StopCoroutine(_activeMove);
+            _activeMove = StartCoroutine(MoveRoutine(targetGridPos, duration, onComplete));
+        }
+
+        private IEnumerator MoveRoutine(Vector2Int targetGridPos, float duration, Action onComplete)
+        {
+            IsMoving = true;
+            Vector3 start = transform.position;
+            Vector3 end = GridCoordinateUtil.GridToWorld(targetGridPos, cellSize);
+            float elapsed = 0f;
+
+            if (duration <= 0f)
+            {
+                transform.position = end;
+            }
+            else
+            {
+                while (elapsed < duration)
+                {
+                    elapsed += Time.deltaTime;
+                    float t = easeCurve.Evaluate(Mathf.Clamp01(elapsed / duration));
+                    transform.position = Vector3.LerpUnclamped(start, end, t);
+                    yield return null;
+                }
+                transform.position = end;
+            }
+
+            IsMoving = false;
+            _activeMove = null;
+            onComplete?.Invoke();
+        }
+    }
+}
--- Assets/scipt/Swipeinputcontroller.cs
+++ Assets/scipt/Swipeinputcontroller.cs
@@ -1,16 +1,68 @@
+using System;
 using UnityEngine;
+using GridPuzzle.Core;
 
-public class Swipeinputcontroller : MonoBehaviour
+namespace GridPuzzle.Input
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+    public class SwipeInputController : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float swipeThreshold = 50f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public event Action<Direction> OnSwipe;
+
+        private Vector2 _touchStart;
+        private bool _tracking;
+
+        private void Update()
+        {
+            HandleTouch();
+            HandleKeyboard();
+        }
+
+        private void HandleTouch()
+        {
+            if (UnityEngine.Input.touchCount == 0) return;
+
+            Touch touch = UnityEngine.Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    _touchStart = touch.position;
+                    _tracking = true;
+                    break;
+
+                case TouchPhase.Ended:
+                case TouchPhase.Canceled:
+                    if (!_tracking) return;
+                    _tracking = false;
+                    Vector2 delta = touch.position - _touchStart;
+                    ClassifyAndFire(delta);
+                    break;
+            }
+        }
+
+        private void HandleKeyboard()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.UpArrow) || UnityEngine.Input.GetKeyDown(KeyCode.W))
+                OnSwipe?.Invoke(Direction.Up);
+            else if (UnityEngine.Input.GetKeyDown(KeyCode.DownArrow) || UnityEngine.Input.GetKeyDown(KeyCode.S))
+                OnSwipe?.Invoke(Direction.Down);
+            else if (UnityEngine.Input.GetKeyDown(KeyCode.LeftArrow) || UnityEngine.Input.GetKeyDown(KeyCode.A))
+                OnSwipe?.Invoke(Direction.Left);
+            else if (UnityEngine.Input.GetKeyDown(KeyCode.RightArrow) || UnityEngine.Input.GetKeyDown(KeyCode.D))
+                OnSwipe?.Invoke(Direction.Right);
+        }
+
+        private void ClassifyAndFire(Vector2 delta)
+        {
+            if (delta.magnitude < swipeThreshold) return;
+
+            if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+                OnSwipe?.Invoke(delta.x > 0 ? Direction.Right : Direction.Left);
+            else
+                OnSwipe?.Invoke(delta.y > 0 ? Direction.Up : Direction.Down);
+        }
     }
 }
